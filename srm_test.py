3f0f9642d0601d12f02da7a6ba3d5d91ee595733
Konstantin Petrov <petroff.cat@gmail.com>
2017-09-18 23:27:28 +1000

    Modified test plot format

--- srm_test.py
+++ srm_test.py
@@ -6,10 +6,10 @@ from pysrm import srm
 
 if __name__ == '__main__':
     img = img_as_ubyte(equalize_hist(io.imread('test.tif')))
-    avg_out, lbl_out = srm.segment(img, q=15)
-    plt.figure()
-    plt.imshow(avg_out, cmap='gray')
-    plt.show()
-    plt.figure()
-    plt.imshow(lbl_out)
+    avg_out, lbl_out = srm.segment(img, q=10)
+    f, ax = plt.subplots(2, 2)
+    ax = ax.ravel()
+    ax[0].imshow(img, cmap='gray')
+    ax[1].imshow(avg_out, cmap='gray')
+    ax[2].imshow(lbl_out)
     plt.show()
